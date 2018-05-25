835382e6cd4cffb71cfec1d91262e386e75bed07
Tim Göller <tim.goeller@outlook.de>
2018-05-25 18:06:53 +0200

    Remove NeuralNetwork constructor for method based initialization

--- src/NeuralNetwork/NeuralNetwork.java
+++ src/NeuralNetwork/NeuralNetwork.java
@@ -8,21 +8,7 @@ public class NeuralNetwork { //static?
     private List<HiddenLayer> hiddenLayers;
     private OutputLayer outputLayer;
 
-    public NeuralNetwork(int inputNeuronCount, int[] hiddenLayerNeuronsCount) {
+    public NeuralNetwork() {
 
-        inputLayer = new InputLayer(inputNeuronCount);
-        outputLayer = new OutputLayer();
-
-        int currentLayer = 0;
-        for(int layerNeuronCount : hiddenLayerNeuronsCount) {
-            if(currentLayer == 0) {
-                hiddenLayers.add(new HiddenLayer(layerNeuronCount, inputNeuronCount));
-            }
-            else {
-                hiddenLayers.add(new HiddenLayer(layerNeuronCount, hiddenLayers.get(currentLayer - 1).getNeuronCount()));
-            }
-
-            currentLayer++;
-        }
     }
 }
